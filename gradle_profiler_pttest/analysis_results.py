14026a06291343de77b2412b84c72a663ac032c1
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-09-19 16:13:58 +0200

    Modeled the result for pttest analysis

---
+++ gradle_profiler_pttest/analysis_results.py
@@ -0,0 +1,20 @@
+# analysis_results.py
+
+from dataclasses import dataclass
+
+from .gradle_benchmark import GradleBenchmark
+
+
+@dataclass
+class PairedTTestDetails:
+    t_statistic: float
+    pvalue: float
+    significance_level: float
+
+
+@dataclass
+class AnalysisResults:
+    baseline: GradleBenchmark
+    candidate: GradleBenchmark
+    details: PairedTTestDetails
+    improvement_detected: bool
