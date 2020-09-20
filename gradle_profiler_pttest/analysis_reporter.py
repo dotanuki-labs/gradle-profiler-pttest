ea28e57cbbe67fef3f8ff8e2ed9339c856ce036d
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-09-20 11:34:31 +0200

    Implemented results reporter

---
+++ gradle_profiler_pttest/analysis_reporter.py
@@ -0,0 +1,71 @@
+# analysis_reporter.py
+
+from rich.console import Console
+from rich.table import Table
+
+
+def report(analysis):
+    benchmarks = format_benchmarks(analysis)
+    results = format_results(analysis)
+    conclusion = format_conclusion(analysis)
+
+    console = Console()
+    console.print("\n🔥 [bold cyan]Provided Samples[/bold cyan]")
+    console.print("\n [bold]Baseline builds[/bold] → ~/Desktop/benchmarks/test01/benchmark.csv")
+    console.print("\n [bold]Modified builds[/bold] → ~/Desktop/benchmarks/test02/benchmark.csv\n")
+
+    console.print("\n🔥 [bold cyan]Details for benchmarks[/bold cyan]\n")
+    console.print(benchmarks)
+    console.print("\n🔥 [bold cyan]Outcomes from the left-tailed Paired T-test[/bold cyan]\n")
+    console.print(results)
+    console.print(conclusion)
+
+
+def format_benchmarks(analysis):
+    h0 = analysis.baseline
+    h1 = analysis.candidate
+
+    benchmarks = Table(show_header=True, header_style="bold magenta")
+    benchmarks.pad_edge = False
+    benchmarks.add_column(analysis.baseline.gradle_task)
+    benchmarks.add_column("Measured builds", justify="right")
+    benchmarks.add_column("Mean", justify="right")
+    benchmarks.add_column("Standard Deviation", justify="right")
+
+    benchmarks.add_row(
+        "baseline (h0)", f"{len(h0.measured_builds)}", h0.mean, h0.standard_deviation
+    )
+
+    benchmarks.add_row(
+        "modified (h1)", f"{len(h1.measured_builds)}", h1.mean, h1.standard_deviation
+    )
+
+    return benchmarks
+
+
+def format_results(analysis):
+    results = Table(show_header=True, header_style="bold magenta")
+    results.pad_edge = False
+    results.add_column("Statistic Metric")
+    results.add_column("Value")
+
+    details = analysis.details
+    results.add_row("t-stastistic", f"{details.t_statistic}")
+    results.add_row("significance level", f"{details.significance_level}")
+    results.add_row("p-value", f"{details.pvalue}")
+
+    return results
+
+
+def format_conclusion(analysis):
+    improved = analysis.improvement_detected
+    accepted_prefix = "\n🔥 [cyan]p-value[/cyan] is lower than [cyan]significance level[/cyan]\n"
+    rejected_prefix = "\n🔥 [cyan]p-value[/cyan] is greater than [cyan]significance level[/cyan]\n"
+    conclusion_posfix = "for improvements with modified build conditions.\n"
+
+    if improved:
+        conclusion = "\nTherefore [green]we have strong statistical evidence[/green]"
+        return f"{accepted_prefix} {conclusion} {conclusion_posfix}"
+    else:
+        conclusion = "\nTherefore we are [red]lacking strong statistical evidence[/red]"
+        return f"{rejected_prefix} {conclusion} {conclusion_posfix}"
--- gradle_profiler_pttest/app.py
+++ gradle_profiler_pttest/app.py
@@ -1,16 +1,28 @@
 # app.py
 
-
-from . import cli_parser
-
-import sys
+from . import analysis_reporter
+from .analysis_results import AnalysisResults
+from .analysis_results import PairedTTestDetails
+from .gradle_benchmark import GradleBenchmark
 
 
 def main(argv=None):
-    answer = cli_parser.parse(argv)
+    test_report()
 
-    if answer == '42':
-        print("Answer is correct!")
-    else:
-        print("This is not the answer for everything in the universe")
-        sys.exit(1)
+
+def test_report():
+    task = 'mobile:assembleDebug'
+
+    baseline_builds = [34123, 36909, 36047, 39181, 33351]
+    baseline_mean = '35922.2'
+    baseline_stddev = '2316.1'
+    baseline = GradleBenchmark(task, baseline_builds, baseline_mean, baseline_stddev)
+
+    candidate_builds = [22123, 26909, 27047, 29181, 23351]
+    candidate_mean = '25722.2'
+    candidate_stddev = '2902.7'
+    candidate = GradleBenchmark(task, candidate_builds, candidate_mean, candidate_stddev)
+
+    details = PairedTTestDetails(4.0, 0.01, 0.05)
+    analysis = AnalysisResults(baseline, candidate, details, True)
+    analysis_reporter.report(analysis)
