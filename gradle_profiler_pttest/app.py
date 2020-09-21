829135f7e2e2f1c18c09a0334e10e8dd06714c72
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-09-21 09:44:39 +0200

    Wiring at app level

--- gradle_profiler_pttest/app.py
+++ gradle_profiler_pttest/app.py
@@ -1,28 +1,14 @@
 # app.py
 
 from . import analysis_reporter
-from .analysis_results import AnalysisResults
-from .analysis_results import PairedTTestDetails
-from .gradle_benchmark import GradleBenchmark
+from . import benchmark_parser
+from . import benchmarks_analyser
+from . import cli_parser
 
 
 def main(argv=None):
-    test_report()
-
-
-def test_report():
-    task = 'mobile:assembleDebug'
-
-    baseline_builds = [34123, 36909, 36047, 39181, 33351]
-    baseline_mean = '35922.2'
-    baseline_stddev = '2316.1'
-    baseline = GradleBenchmark(task, baseline_builds, baseline_mean, baseline_stddev)
-
-    candidate_builds = [22123, 26909, 27047, 29181, 23351]
-    candidate_mean = '25722.2'
-    candidate_stddev = '2902.7'
-    candidate = GradleBenchmark(task, candidate_builds, candidate_mean, candidate_stddev)
-
-    details = PairedTTestDetails(4.0, 0.01, 0.05)
-    analysis = AnalysisResults(baseline, candidate, details, True)
+    baseline_csv, modified_csv = cli_parser.parse(argv)
+    baseline = benchmark_parser.parse(baseline_csv)
+    modified = benchmark_parser.parse(modified_csv)
+    analysis = benchmarks_analyser.analyse(baseline, modified)
     analysis_reporter.report(analysis)
--- gradle_profiler_pttest/benchmarks_analyser.py
+++ gradle_profiler_pttest/benchmarks_analyser.py
@@ -10,9 +10,10 @@ SIGNIFICANCE_LEVEL = 0.05
 
 
 def analyse(baseline, candidate):
-    stats = ttest(baseline.measured_builds, candidate.measured_builds, paired=True, tail='one-sided').round(2)
+    stats = ttest(baseline.measured_builds, candidate.measured_builds, paired=True, tail='greater').round(3)
     tstatistic = stats.loc['T-test', 'T']
     pvalue = stats.loc['T-test', 'p-val']
+    print(stats)
     improvement_detected = pvalue < SIGNIFICANCE_LEVEL
     details = PairedTTestDetails(tstatistic, pvalue, SIGNIFICANCE_LEVEL)
     return AnalysisResults(baseline, candidate, details, improvement_detected)
