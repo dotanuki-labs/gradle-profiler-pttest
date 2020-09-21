0bca351945e1268452c4ce16ae5602593aac63bf
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-09-21 11:26:45 +0200

    Added integration tests for app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,39 @@
 # test_app.py
 
-# from gradle_profiler_pttest import app
+import os
+
+from gradle_profiler_pttest import app
+
+FIXTURES_DIR = f"{os.getcwd()}/tests/fixtures"
 
 
-def test_correct_answer():
+def test_should_reject_improvements_given_benchmarks(capsys):
 
     # Given
-    # argv = ['-a', '42']
+    baseline = f"{FIXTURES_DIR}/sdksearch/jdk8/benchmark.csv"
+    modified = f"{FIXTURES_DIR}/sdksearch/jdk11/benchmark.csv"
 
     # When
-    # app.main(argv)
+    argv = ['-b', baseline, '-m', modified]
+    app.main(argv)
 
     # Then
-    assert True
+    captured = capsys.readouterr()
+    assert "p-value is greater than significance level" in captured.out
+    assert "we are lacking strong statistical evidence for improvements" in captured.out
+
+
+def test_should_accept_improvements_given_benchmarks(capsys):
+
+    # Given
+    baseline = f"{FIXTURES_DIR}/iosched/outdated-agp/benchmark.csv"
+    modified = f"{FIXTURES_DIR}/iosched/updated-agp/benchmark.csv"
+
+    # When
+    argv = ['-b', baseline, '-m', modified]
+    app.main(argv)
+
+    # Then
+    captured = capsys.readouterr()
+    assert "p-value is lower than significance level" in captured.out
+    assert "we have strong statistical evidence for improvements" in captured.out
